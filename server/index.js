c23d89839cbd8cd8fe76f66ca2392b6bddd76f05
Haci11 <46707887+Haci11@users.noreply.github.com>
2022-12-21 15:15:48 +0100

    Added 2 endpoints

--- server/index.js
+++ server/index.js
@@ -1,16 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("Cors");
+const app = express();
+const { Listing } = require("./models/Listing");
 const mongoose = require("mongoose");
-const { Apartment } = require("./models/Apartment");
+mongoose.set("strictQuery", true);
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(bodyParser.json());
 
 const url =
   "mongodb+srv://airbnb:airbnb@cluster0.ihesg3d.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
+mongoose.connect(url, () => {
+  console.log("connected");
+});
 
-async function main() {
-  mongoose.set("strictQuery", true);
-  await mongoose.connect(url);
+app.get("/listings", async (req, res) => {
+  const listings = await Listing.find({}).limit(20);
+  res.json(listings);
+});
 
-  const apartment = await Apartment.find({})
-    .select({ name: 1, price: 1, summary: 1 })
-    .limit(2);
-  console.log(apartment);
-}
-main();
+app.get("/listings/:id", async (req, res) => {
+  id = req.params.id;
+  const listing = await Listing.findOne({ _id: id });
+  res.json(listing);
+});
+
+app.listen(3000);
