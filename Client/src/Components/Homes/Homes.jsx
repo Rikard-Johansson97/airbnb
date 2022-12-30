0d6fa6e92012931bc8569c57506b107a0ed12bdf
Rikard Johansson <Rikard.com@hotmail.com>
2022-12-30 17:34:27 +0100

    Loading Animation
    added loading animation on homepage.
    fixed homepage on logoClick

--- Client/src/Components/Homes/Homes.jsx
+++ Client/src/Components/Homes/Homes.jsx
@@ -8,7 +8,6 @@ import { useQuery } from "../../hooks/useQuery";
 const Homes = ({ searchQuery }) => {
   const [query, setQuery] = useState("");
   const [data, loading] = useQuery(`/listings/${query}`);
-
   useEffect(() => {
     if (searchQuery) {
       console.log(searchQuery);
@@ -20,18 +19,27 @@ const Homes = ({ searchQuery }) => {
     setQuery(`categories/${category}`); // Update the query state when the category changes
   };
 
-  if (loading)
+  if (loading) {
+    const cards = [];
+    for (let index = 0; index < 20; index++) {
+      cards.push(<HomeCard loading={loading} key={index} />);
+    }
     return (
-      <div className='no-result'>
-        <img src={noResult} className='no-result-img' alt='noHomeFound' />
-      </div>
+      <>
+        <Categories onCategoryChange={handleCategoryChange} />
+        <div className='Homes-container'>{cards}</div>
+      </>
     );
+  }
+
   return (
     <>
       <Categories onCategoryChange={handleCategoryChange} />
       <div className='Homes-container'>
         {data.length > 0 ? (
-          data.map((home) => <HomeCard key={home._id} home={home} />)
+          data.map((home) => (
+            <HomeCard loading={loading} key={home._id} home={home} />
+          ))
         ) : (
           <div className='no-result'>
             <img src={noResult} className='no-result-img' alt='noHomeFound' />
