2733c365a35335f2acfeee238231f966ed1f9d94
Rikard Johansson <106700975+Rikard-Johansson97@users.noreply.github.com>
2022-12-30 17:36:14 +0100

    task-5: Loading Animation (#11)
    added loading animation on homepage.
    fixed homepage on logoClick

--- Client/src/Components/Header/Header.jsx
+++ Client/src/Components/Header/Header.jsx
@@ -3,6 +3,7 @@ import "./Header.css";
 import logo from "../../Assets/airbnb-logo-icon-png-svg.png";
 import searchIcon from "../../Assets/search-icon.png";
 import { Link } from "react-router-dom";
+
 const Header = (props) => {
   const searchInput = useRef(null);
 
@@ -14,7 +15,7 @@ const Header = (props) => {
 
   return (
     <header className='header-container'>
-      <Link href='/' className='logo-container' id='home-btn'>
+      <Link to='/' className='logo-container' id='home-btn'>
         <img className='logo' src={logo} alt='logo' />
         <h1 className='logo-text'>airbnb</h1>
       </Link>
