5360f597a82cce4fde65bf1bac2c0f0cf6e35345
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-01-03 14:37:53 +0100

    Task-4

---
+++ Client/src/Components/Detail/Price/Price.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useMedia } from "../../../hooks/useMedia";
+import reviewStar from "../../../Assets/star-icon.png";
+
+const Price = ({ data }) => {
+  const isLarge = useMedia("(min-width: 800px)");
+  const rating =
+    data.review_scores !== undefined
+      ? `${data.review_scores.review_scores_value}.0`
+      : ``;
+  // STAR
+  const star = rating ? (
+    <img src={reviewStar} className="detail-star" alt="star" />
+  ) : (
+    " "
+  );
+  return (
+    <div className="detail-price-container">
+      <div>
+        {isLarge ? (
+          <div className="detail-price">
+            <div>
+              <div className="detail-price-header">
+                <p className="detail-header-title">{data.price} Kr SEK natt</p>
+                <div className="detail-rating-container">
+                  {star}
+                  <p>{rating}</p>
+                </div>
+                <p className="detail-underline">
+                  {data.reviews.length} Omdömen
+                </p>
+              </div>
+              <button className="btn--price large">Reservera</button>
+              <div className="detail-price-footer">
+                <div>
+                  <p className="detail-footer-text">{data.price} X 5 nätter</p>
+                  <p className="detail-footer-text">Städavgift</p>
+                </div>
+                <div>
+                  <p>{data.price * 5} kr SEK</p>
+                  <p>{data.cleaning_fee.$numberDecimal}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="detail-price">
+            <p>{data.price} Kr SEK natt</p>
+
+            <button className="btn--price">Reservera</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Price;
