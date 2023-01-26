197609c4f8b2046028c1bb1d242a28ae95a15b13
Rikard Johansson <Rikard.com@hotmail.com>
2023-01-26 09:17:39 +0100

    Build fix

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./Components/Header/header";
+import Header from "./Components/Header/Header";
 import Homes from "./Components/Homes/Homes";
 import Detail from "./Components/Detail/Detail";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -16,8 +16,8 @@ function App() {
     <BrowserRouter>
       <Header onSearch={handleSearch} />
       <Routes>
-        <Route path="/" element={<Homes searchQuery={searchQuery} />} />
-        <Route path="/listings/:id" element={<Detail />} />
+        <Route path='/' element={<Homes searchQuery={searchQuery} />} />
+        <Route path='/listings/:id' element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
